a7093587cbb74ad682f3f188a55ae8f6a46d2509
Brendan <b.downing.code@gmail.com>
2022-02-11 09:12:46 -0800

    More

--- blank.js
+++ blank.js
@@ -1,12 +1,1 @@
-function errorThrown(message) {
-    console.log(message);
-}
 
-try {
-    const num = 10;
-    if (typeof num !== 'string') {
-        throw new Error('Need a string');
-    }
-} catch (e) {
-    errorThrown(e.message);
-}
