20da18fa2abbb9c126c850ed191b4ddb9c268703
Downster <b.downing.code@gmail.com>
2022-03-25 09:50:11 -0700

    More

--- Searching/bfs.js
+++ Searching/bfs.js
@@ -1,20 +1,20 @@
 const BFS = (tree) => {
-    let queue = [];
-    let result = [];
-    let node = tree.root;
+  let queue = [];
+  let result = [];
+  let node = tree.root;
 
-    queue.push(node);
-    while (queue.length){
-        node = queue.shift()
-        result.push(node.value);
-        if (node.left){
-            queue.push(node.left);
-        }
-        if (node.right){
-            queue.push(node.right);
-        }
+  queue.push(node);
+  while (queue.length) {
+    node = queue.shift();
+    result.push(node.value);
+    if (node.left) {
+      queue.push(node.left);
     }
-    return result;
-}
+    if (node.right) {
+      queue.push(node.right);
+    }
+  }
+  return result;
+};
 
 module.exports = BFS;
